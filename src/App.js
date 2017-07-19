920f4b2eb27ff1ea4ebc5febb24361336fd92727
projectmaking123 <jason.89.wang@gmail.com>
2017-07-19 15:57:49 -0400

    staging for presentation

--- src/App.js
+++ src/App.js
@@ -20,36 +20,25 @@ class App extends Component {
   }
 
   componentDidMount() {
-    database.ref('/messages').on('value', (snapshot) => {
-      this.setState({
-        data: snapshot.val()
-      });
-    });
+
     auth.onAuthStateChanged(currentUser => {
       this.setState({currentUser})
     })
   }
 
   handleChange(event) {
-    this.setState({
-      message: [`${this.state.currentUser.displayName}`, event.target.value],
-    });
+
   }
 
   handleSubmit(event) {
-    event.preventDefault()
-    database.ref(`/messages/${new Date()}`).set(this.state.message)
+
   }
 
   render() {
     return (
       <div>
         <div>
-          {
-            map(this.state.data, (value, key) =>
-              <p key={key}> {value[0]} : {value[1]} </p>
-            )
-          }
+
         </div>
 
         <div>
@@ -70,7 +59,7 @@ class App extends Component {
 
         </pre>
         <form onSubmit={this.handleSubmit}>
-          <input type="text" value={this.state.message[1]} onChange={this.handleChange} />
+          <input type="text" onChange={this.handleChange} />
           <input type="submit" />
         </form>
       </div>
